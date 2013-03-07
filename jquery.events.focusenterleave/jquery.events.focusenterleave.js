3b4959c890ba9d116df727769c8379e48d125464
Enideo <hello@enideo.com>
2013-03-07 09:32:22 +0100

    Dispatch added, Zepto compatible

--- jquery.events.focusenterleave/jquery.events.focusenterleave.js
+++ jquery.events.focusenterleave/jquery.events.focusenterleave.js
@@ -1,59 +1,55 @@
 /**
  * jQuery EnterLeave
  * Special events: focusenter, focusleave
- * @license Copyright 2010 Enideo. Released under dual MIT and GPL licenses.
+ * @license Copyright 2013 Enideo. Released under dual MIT and GPL licenses.
 */
 
 
-(function($) {
+$.event.special.focusenter = {
+  setup: function(data, namespaces) {
+    $(this).bind('focusin.focusenter', $.event.special.focusenter.handler);
+  },
 
-  $.event.special.focusenter = {
-    setup: function(data, namespaces) {
-      $(this).bind('focusin.focusenter', $.event.special.focusenter.handler);
-    },
+  teardown: function(namespaces) {
+    $(this).unbind('focusin.focusenter', $.event.special.focusenter.handler);
+  },
 
-    teardown: function(namespaces) {
-      $(this).unbind('focusin.focusenter', $.event.special.focusenter.handler);
-    },
+  handler: function(event){
+    var $self = $(event.currentTarget);
 
-    handler: function(event){
-      var self = $(event.currentTarget);
+    clearTimeout($self.data('focusleaveTimeout'));
+    $self.removeData('focusleaveTimeout');
 
-      clearTimeout(self.data('focusleaveTimeout'));
-      self.removeData('focusleaveTimeout');
-
-      if( !self.data('focusentered') ){
-        self.data('focusentered',true);
-        event.type = 'focusenter';
-        $.event.handle.apply(this, arguments);
-      }
+    if( !$self.data('focusentered') ){
+      $self.data('focusentered',true);
+      event.type = 'focusenter';
+      ($.event.dispatch || $.event.handle).apply(this, [event]);
     }
-  };
+  }
+};
 
-  $.event.special.focusleave = {
-    setup: function(data, namespaces) {
-      $(this).bind('focusout.focusleave', $.event.special.focusleave.handler);
-    },
+$.event.special.focusleave = {
+  setup: function(data, namespaces) {
+    $(this).bind('focusout.focusleave', $.event.special.focusleave.handler);
+  },
 
-    teardown: function(namespaces) {
-      $(this).unbind('focusout.focusleave', $.event.special.focusleave.handler);
-    },
+  teardown: function(namespaces) {
+    $(this).unbind('focusout.focusleave', $.event.special.focusleave.handler);
+  },
 
-    handler: function(event){
+  handler: function(event){
 
-      var self = event.currentTarget,
-        elem = this,
-        args = arguments,
-        timeout = setTimeout(function(){
+    var $self = $(event.currentTarget),
+      elem = this,
+      timeout = setTimeout(function(){
 
-          event.type = 'focusleave';
-          $.event.handle.apply(elem, args);
-          $(self).removeData('focusentered');
+        event.type = 'focusleave';
+        ($.event.dispatch || $.event.handle).apply(elem, [event]);
+        $self.removeData('focusentered');
 
-        },100);
+      },100);
 
-      $(self).data('focusleaveTimeout',timeout);
-    }
-  };
+    $self.data('focusleaveTimeout',timeout);
 
-})(jQuery);
+  }
+};
